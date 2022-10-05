3977cd10a6d7d5fd86500f7eed070de944759ae5
Cesar Vezga <cesar.vezga@grainger.com>
2022-10-04 19:33:19 -0600

    control console char color

--- beep.py
+++ beep.py
@@ -2,7 +2,7 @@
 # 07 BEL 
 print(chr(7))
 
-# public static final char[] SET_RED_FOREGROUND = {ESC, '[', '3', '1', 'm'}; // 31        set red foreground
+# char[] SET_RED_FOREGROUND = {ESC, '[', '3', '1', 'm'}; // set red foreground
 SET_RED_FOREGROUND = (chr(27),"[","3", "1", "m")
 print("".join(SET_RED_FOREGROUND))
 print("rojo")
