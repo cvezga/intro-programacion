ffbe550e2f444db11b933995af3dc7837712dddd
Cesar Vezga <cesar.vezga@grainger.com>
2022-10-06 20:15:36 -0600

    whatsapp text note

--- whatsapp-send-message.py
+++ whatsapp-send-message.py
@@ -21,10 +21,15 @@ data = '''
 }
 '''
 
+# El destinatario tiene que llamar primero para poder responder
+# IMPORTANT: If a customer has not messaged you first, then the first time you send a message to a user, 
+# WhatsApp requires that the message contains a template. This is explained in more detail in the Understanding WhatsApp topic.
+# ref: https://developer.vonage.com/messages/code-snippets/whatsapp/send-text
+
 data2 =  '''
 { "messaging_product": "whatsapp", 
   "recipient_type": "individual",
-  "to": "50671400049", 
+  "to": "{phone_to}", 
   "type": "text", 
   "text": { 
             "preview_url": false, 
@@ -34,8 +39,9 @@ data2 =  '''
 '''
 
 body = data.replace("{phone_to}", phone_to)
+body2 = data2.replace("{phone_to}", phone_to)
 
-response = requests.post(f'https://graph.facebook.com/v15.0/{phone_from}/messages', headers=headers, data=body)
+response = requests.post(f'https://graph.facebook.com/v15.0/{phone_from}/messages', headers=headers, data=body2)
 
 print(response)
 
